c78d543a4b491c329c0065ada7b60f1e3b0dd258
Martin Alcaraz <tinchoalca13@gmail.com>
2026-05-11 18:28:38 -0300

    Se agregaron los archivos js correspondientes para la logica del Login

---
+++ controllers/autorizacionController.js
@@ -0,0 +1,24 @@
+/*Se declaran las variables fs(File System) y path(Ruta) para simular el logueo de un usuario que se creo en ..data/usuarios.json*/ 
+const fs = require('fs');
+const path = require('path');
+
+const login = (req, res) => {
+    const { email, password } = req.body;
+    
+    // Leemos el archivo JSON
+    const usuarios = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/usuarios.json'), 'utf-8'));
+
+    // Buscamos si existe el usuario y coincide la contraseña
+    const usuarioValido = usuarios.find(u => u.email === email && u.password === password);
+
+    if (usuarioValido) {
+        return res.status(200).json({ 
+            msg: "Correcto", 
+            usuario: usuarioValido.nombre 
+        });
+    } else {
+        return res.status(401).json({ 
+            msg: "Incorrecto" 
+        });
+    }
+};
