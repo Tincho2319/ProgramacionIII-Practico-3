0d9303ea09f9672fe32144d8df3fdd5f18a61a04
Martin Alcaraz <tinchoalca13@gmail.com>
2026-05-13 19:43:51 -0300

    Bug login -- Arreglado

--- controllers/autorizacionController.js
+++ controllers/autorizacionController.js
@@ -3,30 +3,54 @@ const path = require('path');
 
 const login = (req, res) => {
     const { email, password } = req.body;
+    
+    const usuariosPath = path.join(__dirname, '../data/usuarios.json');
+    // Leemos el archivo JSON
+    const usuarios = JSON.parse(fs.readFileSync(usuariosPath, 'utf-8'));
 
-    const usuarios = JSON.parse(
-        fs.readFileSync(
-            path.join(__dirname, '../data/usuarios.json'),
-            'utf-8'
-        )
-    );
-
-    const usuarioValido = usuarios.find(
-        u => u.email === email && u.password === password
-    );
+    // Buscamos si existe el usuario y coincide la contraseña
+    const usuarioValido = usuarios.find(u => u.email === email && u.password === password);
 
     if (usuarioValido) {
-        return res.status(200).json({
-            msg: 'Correcto',
-            usuario: usuarioValido.nombre
+        return res.status(200).json({ 
+            msg: "Correcto", 
+            usuario: usuarioValido.nombre 
+        });
+    } else {
+        return res.status(401).json({ 
+            msg: "Incorrecto" 
         });
     }
-
-    return res.status(401).json({
-        msg: 'Incorrecto'
-    });
 };
 
-module.exports = {
-    login
+module.exports = { login };
+
+
+/*
+const fs = require('fs');
+const path = require('path');
+
+const login = (req, res) => {
+    const { email, password } = req.body;
+    
+    const usuariosPath = path.join(__dirname, '../data/usuarios.json');
+    // Leemos el archivo JSON
+    const usuarios = JSON.parse(fs.readFileSync(usuariosPath, 'utf-8'));
+
+    // Buscamos si existe el usuario y coincide la contraseña
+    const usuarioValido = usuarios.find(u => u.email === email && u.password === password);
+
+    if (usuarioValido) {
+        return res.status(200).json({ 
+            msg: "Correcto", 
+            usuario: usuarioValido.nombre 
+        });
+    } else {
+        return res.status(401).json({ 
+            msg: "Incorrecto" 
+        });
+    }
 };
+
+module.exports = { login };
+*/
